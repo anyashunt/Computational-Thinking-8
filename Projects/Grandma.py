6f0f3edb0fb0bac5685ee41cb90ef6f3952b4596
anyashunt <anyahunt@seattleacademy.org>
2025-03-25 21:28:09 +0000

    test

---
+++ Projects/Grandma.py
@@ -0,0 +1,5 @@
+while True:
+    word = input ("what do you think grandma likes?")
+
+    if "t" in word:
+        print (f"grandma doesn't like")
---
+++ Projects/Project.py
@@ -0,0 +1,22 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+stage.set_background("")
+
+q1 = codesters.Square (100, 100, 200, 'LightYellow')
+q2 = codesters.Square (-100, 100, 200, 'Honeydew')
+q3 = codesters.Square (-100, -100, 200, 'MistyRose')
+q4 = codesters.Square (100, -100, 200, 'AliceBlue')
+
+s1 = codesters.Sprite ("Alani", 100, 100)
+s1.set_size(0.1)
+s2 = codesters.Sprite ("Spotify", -100, -100)
+s2.set_size(0.2)
+s3 = codesters.Sprite ("Brandy", 100, -100)
+s3.set_size(0.05)
+s4 = codesters.Sprite ("Starbucks", -100, 100)
+s4.set_size(0.2)
